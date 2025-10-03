c15b3b5709ff025d4f2f600ff7bc6ff5f0f587f4
Georgij Kokolija <kokolija@yandex.ru>
2025-10-03 22:25:12 +0300

    com 10-03

---
+++ pytest_tests/conftest.py
@@ -0,0 +1,44 @@
+import pytest
+
+from django.test.client import Client
+
+from news.models import News, Comment
+
+
+@pytest.fixture
+def author(django_user_model):  
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):
+    client = Client()
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)
+    return client
+
+
+@pytest.fixture
+def news():
+    return News.objects.first()
+
+
+@pytest.fixture
+def comment(author, news):
+    comment = Comment.objects.create(
+        news=news,
+        text='Текст комментария',
+        author=author,
+    )
+    return comment
